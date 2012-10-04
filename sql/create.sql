5a198a913cb8435f6bc3300579eaf5936d7a6c4c
philippgille <abb653@AKELA.informatik.haw-hamburg.de>
2012-10-04 10:55:32 +0200

    sql angepasst und um drops erweitert

--- sql/create.sql
+++ sql/create.sql
@@ -1,3 +1,26 @@
+drop skripte
+create skripte
+insert dummy data skripte
+
+/*=======================*/
+
+drop table preis cascade constraints;
+drop table kindergartentyp cascade constraints;
+drop table kindergarten cascade constraints;
+drop table elternteil cascade constraints;
+drop table kind cascade constraints;
+drop table gruppe cascade constraints;
+drop table wartelistentyp cascade constraints;
+drop table warteliste cascade constraints;
+
+-- relationen
+
+drop table kindergarten_kindergartentyp cascade constraints;
+drop table kind_gruppe cascade constraints;
+drop table kind_warteliste cascade constraints;
+
+/*=======================*/
+
 create table kindergartentyp
 (ident number not null primary key,
 kindergartentyp varchar2(50));
@@ -25,13 +48,11 @@ maxplaetze number not null,
 kontonummer number not null,
 blz number not null);
 
-
 create Table gruppe
 (ident number not null primary key,
 gruppengroesse number not null,
 kindergarten_id number not null references kindergarten,
 warteliste_id number not null references warteliste);
-
 
 create table preisliste
 (ident number not null primary key,
@@ -43,6 +64,7 @@ create table warteliste
 (ident number not null primary key,
 wartelistentyp varchar2(50) not null unique);
 
+-- relationen
 
 create table kindergarten_kindergartentyp
 (kindergarten_id number not null references kindergarten,
@@ -60,8 +82,10 @@ create table kind_warteliste
 (kind_id number not null references kind,
 warteliste_id number not null);
 
+/*=======================*/
+-- !!!! NOCH NICHT FERTIG !!!!!
 
-insert into elternteil values(1,'Müller',3,'Berliner Tor 5; Hamburg',1500);
+insert into elternteil values(1,'Kirschenmann',3,'Berliner Tor 5; Hamburg',1500);
 insert into elternteil values(2,'Wimmer',3,'Berliner Tor 5; Hamburg',2660);
 insert into elternteil values(3,'Bielenberg',3,'Berliner Tor 5; Hamburg',1432);
 insert into elternteil values(4,'Gille',3,'Berliner Tor 5; Hamburg',1841);
@@ -69,7 +93,7 @@ insert into elternteil values(5,'Krome',3,'Berliner Tor 5; Hamburg',2250);
 
 insert into kind values(1,'Kai','Bielenberg','31.03.2006',12345,1);
 insert into kind values(2,'Olli','Behncke','30.03.2006',12346,2);
-insert into kind values(3,'Frizt','Fritzchen','30.04.2007',12546,3);
+insert into kind values(3,'Friz','Fritzchen','30.04.2007',12546,3);
 
 insert into kindergarten values(1, 'Kindergarten', 'Musterstrasse',20,12345,6789);
 
@@ -80,4 +104,3 @@ insert into gruppe values(1, 20, 1, 1);
 insert into kind_warteliste values(1,1);
 insert into kind_warteliste values(1,2);
 insert into kind_warteliste values(1,3);
-
