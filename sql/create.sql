c6acf6701161b9313bc25c60210e591207573a54
Oliver <oliver.behncke@haw-hamburg.de>
2012-10-08 14:26:50 +0200

    Zusammenführung google-docs und github version

--- sql/create.sql
+++ sql/create.sql
@@ -5,7 +5,7 @@
 /*=======================*/
 
 drop table preisliste cascade constraints;
-drop table kindergartentyp cascade constraints;
+drop table preismodell cascade constraints;
 drop table kindergarten cascade constraints;
 drop table elternteil cascade constraints;
 drop table kind cascade constraints;
@@ -14,15 +14,16 @@ drop table warteliste cascade constraints;
 
 -- relationen
 
-drop table kindergarten_kindergartentyp cascade constraints;
+drop table kindergarten_preismodell cascade constraints;
 drop table kind_gruppe cascade constraints;
-drop table kind_warteliste cascade constraints;
+drop table registrierung cascade constraints;
 
 /*=======================*/
 
-create table kindergartentyp
+create table preismodell
 (ident number not null primary key,
-kindergartentyp varchar2(50));
+bezeichnung varchar2(50),
+dauer number not null);
 
 create table elternteil
 (ident number not null primary key,
@@ -37,7 +38,8 @@ Vorname VarChar(50) not null,
 Nachname VarChar(50) not null,
 Geburtsdatum  Date not null,
 HashValue number not null,
-ElternteilId number references elternteil(Ident));
+Elternteil_id number references elternteil(ident),
+preismodell_id number references preismodell(ident));
 
 create table kindergarten
 (ident number not null primary key,
@@ -54,38 +56,45 @@ wartelistentyp varchar2(50) not null unique);
 create Table gruppe
 (ident number not null primary key,
 gruppengroesse number not null,
-kindergarten_id number not null references kindergarten,
-warteliste_id number not null references warteliste);
+kindergarten_id number not null references kindergarten(ident),
+warteliste_id number not null references warteliste(ident));
 
 create table preisliste
 (ident number not null primary key,
-familiengroesse number not null,
+preismodell_id number not null references preismodell(ident),
 nettoeinkommen number not null,
-preis number not null);
+preis2pers number not null,
+preis3pers number not null,
+preis4pers number not null,
+preis5pers number not null,
+preis6pers number not null);
 
 -- relationen
 
-create table kindergarten_kindergartentyp
-(kindergarten_id number not null references kindergarten,
-kindergartentyp_id number not null,
-Primary Key(kindergarten_id),
-Foreign Key(kindergartentyp_id) references kindergartentyp);
+create table kindergarten_preismodell
+(kindergarten_id number not null references kindergarten(ident),
+preismodell_id number not null references preismodell(ident),
+Primary Key(kindergarten_id, preismodell_id));
 
 create Table kind_gruppe
-(gruppe_id number not null references Gruppe,
-kind_id number not null ,
-Primary Key (gruppe_id),
-Foreign Key(kind_id) references Kind);
+(gruppe_id number not null references Gruppe(ident),
+kind_id number not null references kind(ident),
+Primary Key (gruppe_id, kind_id));
 
-create table kind_warteliste
-(kind_id number not null references kind,
-warteliste_id number not null,
-datum_registrierung TIMESTAMP default sysTIMESTAMP, 
-Primary Key (kind_id),
-Foreign Key (warteliste_id) references warteliste);
+create table registrierung
+(kind_id number not null references kind(ident),
+warteliste_id number not null references warteliste(ident),
+datum_registrierung TIMESTAMP default sysTIMESTAMP,
+primary key (kind_id, warteliste_id));
+
+/*
+create table preismodell_preisliste
+(preismodell_id number not null references preismodell(ident),
+preisliste_id number not null references preisliste(ident),
+Primary Key (preismodell_id, preisliste_id));
+*/
 
 /*=======================*/
--- !!!! NOCH NICHT FERTIG !!!!!
 
 insert into elternteil values(1,'Kirschenmann',3,'Berliner Tor 5; Hamburg',1500);
 insert into elternteil values(2,'Wimmer',3,'Berliner Tor 5; Hamburg',2660);
@@ -93,9 +102,26 @@ insert into elternteil values(3,'Bielenberg',3,'Berliner Tor 5; Hamburg',1432);
 insert into elternteil values(4,'Gille',3,'Berliner Tor 5; Hamburg',1841);
 insert into elternteil values(5,'Krome',3,'Berliner Tor 5; Hamburg',2250);
 
-insert into kind values(1,'Kai','Bielenberg','31.03.2006',12345,1);
-insert into kind values(2,'Olli','Behncke','30.03.2006',12346,2);
-insert into kind values(3,'Friz','Fritzchen','30.04.2007',12546,3);
+insert into preismodell values(1,'a',12);
+insert into preismodell values(2,'b',10);
+insert into preismodell values(3,'c',8);
+insert into preismodell values(4,'d',6);
+insert into preismodell values(5,'e',4);
+insert into preismodell values(6,'f',5);
+insert into preismodell values(7,'g',7);
+insert into preismodell values(8,'h',5);
+insert into preismodell values(9,'i',3);
+insert into preismodell values(10,'j',2);
+
+insert into preisliste values(1,1,1111,112,113,114,115,116);
+/*
+insert into preismodell_preisliste values(1,1);
+insert into preismodell_preisliste values(2,1);
+insert into preismodell_preisliste values(3,1);
+*/
+insert into kind values(1,'Kai','Bielenberg','31.03.2006',12345,1,1);
+insert into kind values(2,'Olli','Behncke','30.03.2006',12346,2,2);
+insert into kind values(3,'Friz','Fritzchen','30.04.2007',12546,3,3);
 
 insert into kindergarten values(1, 'Kindergarten', 'Musterstrasse',20,12345,6789);
 
@@ -107,13 +133,18 @@ insert into warteliste values(5, 'ganztags');
 
 insert into gruppe values(1, 20, 1, 1);
 
-insert into kind_warteliste (kind_id,warteliste_id) values(1,1);
-insert into kind_warteliste (kind_id,warteliste_id) values(2,1);
-insert into kind_warteliste (kind_id,warteliste_id) values(3,1);
+insert into registrierung (kind_id,warteliste_id) values(1,1);
+insert into registrierung (kind_id,warteliste_id) values(2,1);
+insert into registrierung (kind_id,warteliste_id) values(3,1);
 
-insert into kind_warteliste (kind_id,warteliste_id) values(3,2);
-insert into kind_warteliste (kind_id,warteliste_id) values(2,2);
-insert into kind_warteliste (kind_id,warteliste_id) values(1,2);
+insert into registrierung (kind_id,warteliste_id) values(3,2);
+insert into registrierung (kind_id,warteliste_id) values(2,2);
+insert into registrierung (kind_id,warteliste_id) values(1,2);
+
+/*====================*/
+
+
+
+/*====================*/
 
 commit;
-
