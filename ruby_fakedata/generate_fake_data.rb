f201ba558770a4b83d368456ae868905d63b104e
Oliver <oliver.behncke@googlemail.com>
2012-11-30 16:12:26 +0100

    Fixed duplicate kindergartennames problem

--- ruby_fakedata/generate_fake_data.rb
+++ ruby_fakedata/generate_fake_data.rb
@@ -7,9 +7,13 @@ require "fileutils"
 if(File.exists?("kindergarten.csv"))
 	FileUtils.mv("kindergarten.csv","kindergarten_#{Time.now.to_i}.csv")
 end
+
+kindergartens=Hash.new {0}
 File.open("kindergarten.csv","w"){|f|
 	ARGV[0].to_i.times{|x|
-	f.puts((Faker::Name.last_name + "_kg").gsub("'",""))
+	name=Faker::Name.last_name
+	kindergartens[name]=kindergartens[name]+1
+	f.puts(("kg_" + name + kindergartens[name].to_s).gsub("'",""))
 	}
 }
 
