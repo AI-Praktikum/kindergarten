6091785bfdabb2a593b08c749e27b19d8ea228f8
Oliver <oliver.behncke@googlemail.com>
2012-11-30 18:43:37 +0100

    added timing and continue when connection is lost

--- ruby_fakedata/create_databases.rb
+++ ruby_fakedata/create_databases.rb
@@ -20,75 +20,84 @@ create_db_stmts=["CREATE TABLE `preismodell` (`ident` bigint(20) NOT NULL AUTO_I
 		"CREATE TABLE `kind_gruppe` (  `gruppe_id` bigint(20) NOT NULL,  `kind_id` bigint(20) NOT NULL,  FOREIGN KEY (`gruppe_id`) REFERENCES `gruppe`(ident),  FOREIGN KEY (`kind_id`) REFERENCES `kind`(ident),  PRIMARY KEY (`kind_id`, `gruppe_id`))",
 		"CREATE TABLE `registrierung` (  `kind_id` bigint(20) NOT NULL,  `warteliste_id` bigint(20) NOT NULL,  `datum_registrierung` TIMESTAMP NOT NULL default now(),  FOREIGN KEY (`kind_id`) REFERENCES `kind`(ident),  FOREIGN KEY (`warteliste_id`) REFERENCES `warteliste`(ident),  PRIMARY KEY (`kind_id`, `warteliste_id`))"]
 
-con = Mysql.new(host,username, password)
+starttime=Time.now
+puts starttime
 File.open("kindergarten.csv","r"){|file|
 	file.each_line { |kindergarten|
-		puts "Kindergarten: " + kindergarten
-		sql_db = "create database if not exists #{kindergarten}"
-		pst_db = con.prepare(sql_db)
-		pst_db.execute
-		pst_db.close
-		grant = "grant all on #{kindergarten}.* to #{kindergarten}@'%' identified by '#{kindergarten}'"
-		pst_grant= con.prepare(grant)
-		pst_grant.execute
-		pst_grant.close
-		con.query("use #{kindergarten}")
-		create_db_stmts.each{|stmt|
-			con.query(stmt)
-		}
-		#inserting
+		begin
+			con = Mysql.new(host,username, password)
+			puts "Kindergarten: " + kindergarten
+			sql_db = "create database if not exists #{kindergarten}"
+			pst_db = con.prepare(sql_db)
+			pst_db.execute
+			pst_db.close
+			grant = "grant all on #{kindergarten}.* to #{kindergarten}@'%' identified by '#{kindergarten}'"
+			pst_grant= con.prepare(grant)
+			pst_grant.execute
+			pst_grant.close
+			con.query("use #{kindergarten}")
+			create_db_stmts.each{|stmt|
+				con.query(stmt)
+			}
+			#inserting
 		
-		#elternteil
-		((children_per_group*groups)+children_in_queue).times{|x|
-			con.query("insert into `elternteil` values('','#{con.escape_string(Faker::Name.name)}',#{1+rand(5)},'#{con.escape_string(Faker::Address.street_address)}',#{1000+rand(2000)},null)")
-		}
+			#elternteil
+			((children_per_group*groups)+children_in_queue).times{|x|
+				con.query("insert into `elternteil` values('','#{con.escape_string(Faker::Name.name)}',#{1+rand(5)},'#{con.escape_string(Faker::Address.street_address)}',#{1000+rand(2000)},null)")
+			}
 				
-		#preismodell: let's get rid of that
-		con.query "insert into `preismodell` values(1,'a',12);"
-		con.query "insert into `preismodell` values(2,'b',10);"
-		con.query "insert into `preismodell` values(3,'c',8);"
-		con.query "insert into `preismodell` values(4,'d',6);"
-		con.query "insert into `preismodell` values(5,'e',4);"
-		con.query "insert into `preismodell` values(6,'f',5);"
-		con.query "insert into `preismodell` values(7,'g',7);"
-		con.query "insert into `preismodell` values(8,'h',5);"
-		con.query "insert into `preismodell` values(9,'i',3);"
-		con.query "insert into `preismodell` values(10,'j',2);"
+			#preismodell: let's get rid of that
+			con.query "insert into `preismodell` values(1,'a',12);"
+			con.query "insert into `preismodell` values(2,'b',10);"
+			con.query "insert into `preismodell` values(3,'c',8);"
+			con.query "insert into `preismodell` values(4,'d',6);"
+			con.query "insert into `preismodell` values(5,'e',4);"
+			con.query "insert into `preismodell` values(6,'f',5);"
+			con.query "insert into `preismodell` values(7,'g',7);"
+			con.query "insert into `preismodell` values(8,'h',5);"
+			con.query "insert into `preismodell` values(9,'i',3);"
+			con.query "insert into `preismodell` values(10,'j',2);"
 		
-		#kindergarten
-		con.query "insert into `kindergarten` values('','#{kindergarten}', 'Musterstrasse',20,12345,6789)"
+			#kindergarten
+			con.query "insert into `kindergarten` values('','#{kindergarten}', 'Musterstrasse',20,12345,6789)"
 		
-		#kinder
-		((children_per_group*groups)+children_in_queue).times{|x|
-			x=x+1
-			birthday=(2005+rand(3)).to_s + "." + (1+rand(11)).to_s + "." + (1+rand(25)).to_s
-			con.query "insert into `kind` values('','#{con.escape_string(Faker::Name.first_name)}','#{con.escape_string(Faker::Name.last_name)}','#{birthday}',#{x},#{x},#{1+rand(9)})"
-		}
+			#kinder
+			((children_per_group*groups)+children_in_queue).times{|x|
+				x=x+1
+				birthday=(2005+rand(3)).to_s + "." + (1+rand(11)).to_s + "." + (1+rand(25)).to_s
+				con.query "insert into `kind` values('','#{con.escape_string(Faker::Name.first_name)}','#{con.escape_string(Faker::Name.last_name)}','#{birthday}',#{x},#{x},#{1+rand(9)})"
+			}
 		
-		#wartelisten
-		con.query "insert into `warteliste` values(1, 'frueh');"
-		con.query "insert into `warteliste` values(2, 'vormittags');"
-		con.query "insert into `warteliste` values(3, 'nachmittags');"
-		con.query "insert into `warteliste` values(4, 'spaet');"
-		con.query "insert into `warteliste` values(5, 'ganztags');"
+			#wartelisten
+			con.query "insert into `warteliste` values(1, 'frueh');"
+			con.query "insert into `warteliste` values(2, 'vormittags');"
+			con.query "insert into `warteliste` values(3, 'nachmittags');"
+			con.query "insert into `warteliste` values(4, 'spaet');"
+			con.query "insert into `warteliste` values(5, 'ganztags');"
 			
-		#gruppe
-		groups.times{|x|
-			con.query "insert into `gruppe` values('',#{max_children_per_group}, '#{con.escape_string(Faker::Name.first_name)}', 1, #{(1+rand(4))});"
-		}
+			#gruppe
+			groups.times{|x|
+				con.query "insert into `gruppe` values('',#{max_children_per_group}, '#{con.escape_string(Faker::Name.first_name)}', 1, #{(1+rand(4))});"
+			}
 		
-		#kind_gruppe
-		(children_per_group*groups).times{|x|
-			x=x+1
-			con.query "insert into `kind_gruppe` values(#{(x%groups)+1},#{x})"
-		}
+			#kind_gruppe
+			(children_per_group*groups).times{|x|
+				x=x+1
+				con.query "insert into `kind_gruppe` values(#{(x%groups)+1},#{x})"
+			}
 		
-		#registrierung
-		to=((children_per_group*groups)+children_in_queue)
-		from=(children_per_group*groups)
-		(from..to).each{|x|
-			con.query "insert into `registrierung` (`kind_id`,`warteliste_id`) values(#{x},#{(x%5)+1})"
-		}
+			#registrierung
+			to=((children_per_group*groups)+children_in_queue)
+			from=(children_per_group*groups)
+			(from..to).each{|x|
+				con.query "insert into `registrierung` (`kind_id`,`warteliste_id`) values(#{x},#{(x%5)+1})"
+			}
+		rescue Mysql::Error => e
+            		puts "could not create database " + kindergarten + " because of " + e.to_s
+	        ensure
+	            con.close if con
+        	end
 	}
 }
-con.close
+stoptime=Time.now
+puts "Creating the databases took me " + ((starttime-stoptime)/60).to_s + " minutes"
